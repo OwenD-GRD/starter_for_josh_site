690082a78c57410303403353c3a5ec79df286839
OwenD-GRD <workowend@gmail.com>
2023-03-17 10:03:05 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="container-fluid text-center">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-6">
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+      </div> <!-- col-md-6 -->
+
+      <div class="col-md-6">
+        <p>Terms and Conditions</p>
+      </div> <!-- col-md-6 -->
+
+    </div> <!-- row -->
+  </div> <!-- container -->
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
