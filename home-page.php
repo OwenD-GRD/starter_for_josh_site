d9a33dde7ad07c27d2c078ddbcb2cdd413a50a16
OwenD-GRD <workowend@gmail.com>
2023-04-08 16:39:25 +0800

    Update home-page.php

--- home-page.php
+++ home-page.php
@@ -17,41 +17,32 @@
 
 <section class="container-fluid articlesbg">
   <div class="container">
-    <h3 class="text-center">LATEST ARTICLES</h3>
+    <h3 class="text-center"><?php the_field('titleforarticles'); ?></h3>
 
     <div class="row">
 
       <div class="col-md-4">
-        <img class="img-fluid" src="http://170.187.231.66/~mesh6/wp-content/themes/starter_for_josh_site/images/port1.jpg" alt="game character">
-        <h4 class="article-title">A PORTRAIT</h4>
-        <p class="date">Friday,November 10. 2030</p> 
-        <p class="article-p">STS has never been shy of seeking new terrains of  investigation. 
-          More and  more  STS s cholars are  starting to explore and intervene in the arts.  
-          This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   
-          We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>  
-        <a class="readmore" href="#"> CONTINUE READING </a> 
+        <img class="img-fluid" src ="<?php the_field('articleimage');?>" alt="game character">
+        <h4><?php the_field('articletitle'); ?></h4>
+        <p class="date"><?php the_field('articledate'); ?></p> 
+        <p><?php the_field('articleexceprt'); ?></p>
+        <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a> 
       </div> <!-- col-md-4 -->
 
       <div class="col-md-4">
-        <img class="img-fluid" src="http://170.187.231.66/~mesh6/wp-content/themes/starter_for_josh_site/images/port2.jpg" alt="game character armour">
-        <h4 class="article-title">A PORTRAIT</h4>
-        <p class="date">Friday,November 10. 2030</p> 
-        <p class="article-p">STS has never been shy of seeking new terrains of  investigation. 
-          More and  more  STS s cholars are  starting to explore and intervene in the arts.  
-          This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   
-          We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>  
-        <a class="readmore" href="#"> CONTINUE READING </a> 
+        <img class="img-fluid" src ="<?php the_field('articleimagetwo');?>" alt="game character">
+        <h4><?php the_field('articletitle'); ?></h4>
+        <p class="date"><?php the_field('articledate'); ?></p> 
+        <p><?php the_field('articleexceprt'); ?></p>  
+        <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a> 
       </div> <!-- col-md-4 -->
 
       <div class="col-md-4">
-        <img class="img-fluid" src="http://170.187.231.66/~mesh6/wp-content/themes/starter_for_josh_site/images/port3.jpg" alt="game map">
-        <h4 class="article-title">A PORTRAIT</h4>
-        <p class="date">Friday,November 10. 2030</p> 
-        <p class="article-p">STS has never been shy of seeking new terrains of  investigation. 
-          More and  more  STS s cholars are  starting to explore and intervene in the arts.  
-          This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   
-          We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts.</p>  
-        <a class="readmore" href="#"> CONTINUE READING </a> 
+      <img class="img-fluid" src ="<?php the_field('articleimagethree');?>" alt="game character">
+      <h4><?php the_field('articletitle'); ?></h4>
+      <p class="date"><?php the_field('articledate'); ?></p> 
+      <p><?php the_field('articleexceprt'); ?></p>  
+      <a href="<?php the_field('readmore'); ?>" class="readmore">CONTINUE READING</a> 
       </div> <!-- col-md-4 -->
 
     </div> <!-- row -->
